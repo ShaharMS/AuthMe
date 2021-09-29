443f9b0fdac3594a5b79b81f3687d8feb95dc29c
ShaharMS <88977041+ShaharMS@users.noreply.github.com>
2021-09-29 09:20:09 +0300

    Optimized and changed name - FlxTextButton

--- src/msf/util/FlxTextButton.hx
+++ src/msf/util/FlxTextButton.hx
@@ -1,35 +1,24 @@
 package msf.util;
 
 import flixel.ui.FlxButton;
-import flixel.addons.weapon.FlxBullet;
 import flixel.FlxG;
 import flixel.text.FlxText;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
-import flixel.group.FlxSpriteGroup;
 
 
 /**
  * A text that calls a function when clicked
+ * Behaves like a regular `FlxText`, but
+ * with extra button functions.
  */
-class MsfTextButton extends FlxText {
+class FlxTextButton extends FlxText {
     public var status:Int;
     var OnClick:Void -> Void;
-    var ogx:Float;
-    var ogy:Float;
-    var ogsize:Int;
-    var ogcolor:Int;
-    var ogtext:String;
-    var ogfont:String;
+    
 
     public function new(x:Float = 0, y:Float = 0, size:Int = 40, color:Int = FlxColor.WHITE,  text:String = "" ,?font:String = "assets/fonts/OpenSans.ttf", OnClick:Void -> Void) {
         super(x,y);
-        super.x = ogx = x;
-        super.y = ogy = y;
-        super.size = ogsize = size;
-        super.color = ogcolor = color;
-        super.text = ogtext = text;
-        super.font = ogfont = "assets/fonts/OpenSans.ttf";
         this.OnClick = OnClick;
         if (FlxG.mouse.overlaps(this) && FlxG.mouse.justReleased)
             OnClick();
@@ -40,33 +29,15 @@ class MsfTextButton extends FlxText {
         if(FlxG.mouse.overlaps(this) && FlxG.mouse.pressed)
         {    
             status = FlxButton.PRESSED;
-            super.x = ogx;
-            super.y = ogy;
-            super.size = ogsize;
-            super.color = ogcolor;
-            super.text = ogtext;
-            super.font = "assets/fonts/OpenSans.ttf";
         }
         else if (FlxG.mouse.overlaps(this))
         {
             status = FlxButton.HIGHLIGHT;
-            super.x = ogx;
-            super.y = ogy;
-            super.size = ogsize;
-            super.color = ogcolor;
-            super.text = ogtext;
-            super.font = "assets/fonts/OpenSans.ttf";
         }
 
         else
         {
             status = FlxButton.NORMAL;
-            super.x = ogx;
-            super.y = ogy;
-            super.size = ogsize;
-            super.color = ogcolor;
-            super.text = ogtext;
-            super.font = "assets/fonts/OpenSans.ttf";
         }
         
         
