52c2b867b1fc716e673b7946b11372e0a510f9f4
ShaharMS <shaharmsecond@gmail.com>
2021-09-29 23:12:12 +0300

    Update FlxTextButton.hx

--- src/msf/util/FlxTextButton.hx
+++ src/msf/util/FlxTextButton.hx
@@ -15,14 +15,12 @@ import flixel.util.FlxColor;
 class FlxTextButton extends FlxText {
     public var status:Int;
     var OnClick:Void -> Void;
-    public var button:Array<Void -> Void>;
     private var buttonActive:Bool = true;
 
     public function new(x:Float = 0, y:Float = 0, size:Int = 40, color:Int = FlxColor.WHITE,  text:String = "" ,?font:String = "assets/fonts/OpenSans.ttf", OnClick:Void -> Void) {
         super(x,y);
         this.OnClick = OnClick;
-        button.push(enable);
-        button.push(disable);
+        
         if (FlxG.mouse.overlaps(this) && FlxG.mouse.justReleased)
             OnClick();
     }
