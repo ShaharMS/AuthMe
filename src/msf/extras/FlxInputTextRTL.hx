bfcec47f632761648f89fc4ac4b4b88f8553c2f5
ShaharMS <shaharmsecond@gmail.com>
2021-11-18 20:44:41 +0200

    fixed FlxInputTextRTL

--- src/msf/extras/FlxInputTextRTL.hx
+++ src/msf/extras/FlxInputTextRTL.hx
@@ -2,6 +2,7 @@ package msf.extras;
 
 import flixel.addons.ui.FlxInputText;
 import openfl.events.KeyboardEvent;
+import flixel.FlxG;
 /**
  * FlxInputText with support for RTL languages.
  */
@@ -34,44 +35,29 @@ class FlxInputTextRTL extends FlxInputText
 		text = insertSubstring(text, ",", caretIndex);
 		trace("periodpress");
 	}
-
-	public function pressFSlash()
-	{
-		caretIndex = text.length;
-		text = insertSubstring(text, "/", caretIndex);
-		trace("periodpress");
-	}
-
-	public function pressBSlash()
-	{
-		caretIndex = text.length;
-		text = insertSubstring(text, '"\"', caretIndex);
-		trace("periodpress");
-	}
 	
-	override function onKeyDown(e:KeyboardEvent)
-	{
+	override function onKeyDown(e:KeyboardEvent) {
 		// most of this is from the overriden void but the actual char code entry is altered
 		var key:Int = e.keyCode;
 
-		
-		
-		if (hasFocus)
-		{
+		if (hasFocus) {
 			var overridenString = mapCharCode(e.charCode);
-			if (overridenString == null)
-			{
+			if (overridenString == null) {
 				// not mapped, do default handling
 				super.onKeyDown(e);
-			}
-			else
-			{
-				var newText:String = overridenString;			
+			} else {
+				var newText:String = overridenString;
 
-				if (newText.length > 0 && (maxLength == 0 || (text.length + newText.length) < maxLength))
-				{
+				// if (newText.length > 0 && (maxLength == 0 || (text.length + newText.length) < maxLength))
+				// {
+				//	text = insertSubstring(text, newText, text.length - caretIndex);
+				//	caretIndex--;
+				//	onChange(FlxInputText.INPUT_ACTION);
+				// }
+
+				if (newText.length > 0 && (maxLength == 0 || (text.length + newText.length) < maxLength)) {
 					text = insertSubstring(text, newText, caretIndex);
-					caretIndex++;
+					caretIndex--;
 					onChange(FlxInputText.INPUT_ACTION);
 				}
 			}
@@ -79,12 +65,40 @@ class FlxInputTextRTL extends FlxInputText
 	}
 
 	var charMap:Map<Int, String> = [
-		113 => "/", 119 => "'", 101 => "ק", 114 => "ר", 116 => "א", 121 => "ט", 117 => "ו", 105 => "ן", 111 => "ם", 112 => "פ", 97 => "ש", 115 => "ד",
-		100 => "ג", 103 => "ע", 104 => "י", 106 => "ח", 107 => "ל", 108 => "ך", 59 => "ף", 122 => "ז", 120 => "ס", 99 => "ב", 118 => "ה",
-		98 => "נ", 110 => "מ", 109 => "צ", 44 => "ת", 46 => "ץ", 102 => "כ", 32 => "", 47 => " ", 39 => "", 63 => ""
+		113 => "/",
+		119 => "'",
+		101 => "ק",
+		114 => "ר",
+		116 => "א",
+		121 => "ט",
+		117 => "ו",
+		105 => "ן",
+		111 => "ם",
+		112 => "פ",
+		97 => "ש",
+		115 => "ד",
+		100 => "ג",
+		103 => "ע",
+		104 => "י",
+		106 => "ח",
+		107 => "ל",
+		108 => "ך",
+		59 => "ף",
+		122 => "ז",
+		120 => "ס",
+		99 => "ב",
+		118 => "ה",
+		98 => "נ",
+		110 => "מ",
+		109 => "צ",
+		44 => "ת",
+		46 => "ץ",
+		102 => "כ",
+		32 => "",
+		47 => "",
+		39 => "",
+		63 => ""
 	];
-
-
 
 
 
@@ -92,5 +106,26 @@ class FlxInputTextRTL extends FlxInputText
 	{
 		trace('trying to map ${charCode}');
 		return charMap[charCode];
+	}
+
+	public override function update(elapsed:Float) {
+		super.update(elapsed);
+		if (hasFocus) {
+			if (FlxG.keys.justPressed.SPACE) {
+				pressSpace();
+			}
+
+			if (FlxG.keys.justPressed.SLASH && FlxG.keys.pressed.SHIFT) {
+				pressQMark();
+			}
+
+			if (FlxG.keys.justPressed.SLASH && !FlxG.keys.pressed.SHIFT) {
+				pressPeriod();
+			}
+
+			if (FlxG.keys.justPressed.QUOTE) {
+				pressComma();
+			}
+		}
 	}
 }
