3b79f9e8732595c169719bd6b22907bd421c2be0
ShaharMS <shaharmsecond@gmail.com>
2021-11-21 20:50:48 +0200

    fixed deleting& moving functions

--- src/msf/extras/FlxInputTextRTL.hx
+++ src/msf/extras/FlxInputTextRTL.hx
@@ -27,6 +27,10 @@ class FlxInputTextRTL extends FlxInputText
 		alignment = FlxTextAlign.RIGHT;
 		this.fieldBorderSprite.visible = false;
 	}
+
+	public function addBorder() {
+		this.fieldBorderSprite.visible = true;
+	}
 	final function pressSpace()
 	{
 		text = insertSubstring(text, " ", caretIndex);
@@ -61,18 +65,44 @@ class FlxInputTextRTL extends FlxInputText
 	
 	
 	override function onKeyDown(e:KeyboardEvent) {
-		// most of this is from the overriden void but the actual char code entry is altered
+		// some of this is from the overriden void but the actual char code entry is altered
 		var key:Int = e.keyCode;
 
 		if (hasFocus) 
 		{
 			var overridenString = mapCharCode(e.charCode);
-			if (overridenString == null) 
+			if (overridenString == null && key != 8 && key != 46 && key != 36 && key != 35) 
 			{
 				// not mapped, do default handling
 				super.onKeyDown(e);
-			} 
-			
+			}
+			// delete is swapped with backspace to give the correct deletion effect
+			else if (key == 46) {
+				if (caretIndex > 0) {
+					caretIndex--;
+					text = text.substring(0, caretIndex) + text.substring(caretIndex + 1);
+					onChange(FlxInputText.BACKSPACE_ACTION);
+				}
+			}
+			// The same with backspace:
+			else if (key == 8) {
+				if (text.length > 0 && caretIndex < text.length) {
+					text = text.substring(0, caretIndex) + text.substring(caretIndex + 1);
+					onChange(FlxInputText.DELETE_ACTION);
+				}
+			}
+			//end key
+			else if (key == 36) {
+				caretIndex = text.length;
+				text = text; // forces scroll update
+			}
+			//home key
+			else if (key == 35) {
+				caretIndex = 0;
+				text = text; // forces scroll update
+			}
+			//if the charCode is a typeable letter, try and insert it at the start of the LTR string,
+			//therefore being inserted correctly
 			else 
 			{
 				var newText:String = overridenString;
@@ -148,10 +178,6 @@ class FlxInputTextRTL extends FlxInputText
 
 			if (FlxG.keys.justPressed.QUOTE) {
 				pressComma();
-			}
-
-			if (FlxG.keys.justPressed.BACKSPACE) {
-				pressBackSpace();
 			}
 		}
 	}
