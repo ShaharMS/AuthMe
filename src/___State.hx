8bd21b93ce345183dbc6bd5b0a4c23aa4e585c22
ShaharMS <shaharmsecond@gmail.com>
2022-01-29 18:45:29 +0200

    implementing a special fix for JS

--- src/___State.hx
+++ src/___State.hx
@@ -2,14 +2,10 @@ package;
 
 import flixel.system.FlxAssets;
 import msf.extras.FlxInputTextRTL;
-import flixel.FlxG;
-import msf.extras.FlxTextFeildRTL;
-import msf.extras.FlxSuperText;
 import flixel.FlxState;
 
 class ___State extends FlxState{
-    
-	var t = new FlxTextFeildRTL();
+
     public override function create() {
         super.create();
         FlxAssets.FONT_DEFAULT = "assets/data/VarelaRound.ttf";
@@ -19,11 +15,7 @@ class ___State extends FlxState{
 
     public override function update(elapsed:Float) {
         super.update(elapsed);
-        if (FlxG.keys.justPressed.ENTER) {
-            var i = t.textFieldSprite;
-			i.y = 200;
-            add(i);
-        }
+        
             
     }
 }
--- src/msf/extras/FlxInputTextRTL.hx
+++ src/msf/extras/FlxInputTextRTL.hx
@@ -1,20 +1,139 @@
 package msf.extras;
-#if FLX_KEYBOARD
+
 import flixel.FlxBasic;
-import flixel.text.FlxText.FlxTextAlign;
 import flixel.addons.ui.FlxInputText;
 import openfl.events.KeyboardEvent;
 import flixel.util.FlxColor;
 import flixel.FlxG;
 
 using flixel.util.FlxStringUtil;
+#if js
+/**
+ * FlxInputText with support for RTL languages.
+ */
+class FlxInputTextRTL extends FlxInputText
+{
+	/**
+	 * the input with which were going to capture key presses.
+	 */
+	var textInput:js.html.InputElement;
+	/**
+	 * word hack to know where to place the caret on language switch
+	 */
+	var __rtlOffset = 0;
+
+	/**
+	 * another hack to allow RTL to LTR convertion to be smoother by fixing the caret position after the mouse moves the caret
+	 */
+	var __lastCaretPosition = 0;
+
+	public var isRtl(default, null):Bool;
+
+	/**
+	 * @param	X				The X position of the text.
+	 * @param	Y				The Y position of the text.
+	 * @param	Width			The width of the text object (height is determined automatically).
+	 * @param	Text			The actual text you would like to display initially.
+	 * @param   size			Initial size of the font
+	 * @param	TextColor		The color of the text
+	 * @param	BackgroundColor	The color of the background (FlxColor.TRANSPARENT for no background color)
+	 * @param	EmbeddedFont	Whether this text field uses embedded fonts or not
+	 */
+	public function new(X:Float = 0, Y:Float = 0, Width:Int = 150, ?Text:String, size:Int = 8, TextColor:Int = FlxColor.BLACK,
+			BackgroundColor:Int = FlxColor.WHITE, EmbeddedFont:Bool = true)
+	{
+		super(X, Y, Width, Text, size, TextColor, BackgroundColor, EmbeddedFont);
+		wordWrap = true;
+		getInput();
+	}
+
+	override function set_hasFocus(newFocus:Bool):Bool
+	{
+		if (newFocus)
+		{
+			if (hasFocus != newFocus)
+			{
+				_caretTimer = new flixel.util.FlxTimer().start(0.5, toggleCaret, 0);
+				caret.visible = true;
+				caretIndex = text.length;
+			}
+			else
+			{
+				__rtlOffset = __lastCaretPosition - getCaretIndex();
+			}
+		}
+		else
+		{
+			// Graphics
+			caret.visible = false;
+			if (_caretTimer != null)
+			{
+				_caretTimer.cancel();
+			}
+		}
+
+		if (newFocus != hasFocus)
+		{
+			calcFrame();
+		}
+		return hasFocus = newFocus;
+	}
+
+	override function onKeyDown(e:KeyboardEvent)
+	{
+		return;
+	}
+
+	function getInput()
+	{
+		textInput = cast js.Browser.document.createElement('input');
+		textInput.type = 'text';
+		textInput.style.position = 'absolute';
+		textInput.style.opacity = "0";
+		textInput.style.color = "transparent";
+		textInput.value = String.fromCharCode(127);
+		textInput.style.left = "0px";
+		textInput.style.top = "50%";
+		untyped (textInput.style).pointerEvents = 'none';
+		textInput.style.zIndex = "-10000000";
+		js.Browser.document.body.appendChild(textInput);
+		textInput.addEventListener('input', (e:js.html.InputEvent) ->
+		{
+			if (hasFocus) {
+				if (e.which == 8) {
+					if (caretIndex > 0)
+					{
+						caretIndex--;
+						text = text.substring(0, caretIndex) + text.substring(caretIndex + 1);
+						onChange(FlxInputText.BACKSPACE_ACTION);
+					}
+				}
+				var char = textInput.value;
+				text += char;
+				text = text;
+			}
+			
+		}, true);
+	}
+
+	function updateFocus()
+	{
+		textInput.focus();
+		textInput.select();
+	}
+
+	public override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+		updateFocus();
+	}
+}
+#else
 /**
  * FlxInputText with support for RTL languages.
  */
 class FlxInputTextRTL extends FlxInputText 
 {
-
-	public var extraUtils:ExtraUtils;
 	/**
 	 * word hack to know where to place the caret on language switch
 	 */
@@ -40,10 +159,7 @@ class FlxInputTextRTL extends FlxInputText
 	public function new(X:Float = 0, Y:Float = 0, Width:Int = 150, ?Text:String, size:Int = 8,startEnglish:Bool = true, TextColor:Int = FlxColor.BLACK, BackgroundColor:Int = FlxColor.WHITE, EmbeddedFont:Bool = true) {
 		super(X, Y, Width, Text, size, TextColor, BackgroundColor, EmbeddedFont);
 		if (startEnglish) {isRtl = false; alignment = LEFT;} else {isRtl = true; alignment = RIGHT;}
-
-		extraUtils = new ExtraUtils(this);
 		wordWrap = true;
-
 	}
 
 	final function pressSpace()
@@ -80,7 +196,6 @@ class FlxInputTextRTL extends FlxInputText
 	
 	override function onKeyDown(e:KeyboardEvent) {
 
-		
 		if (e.altKey && e.shiftKey) {
 			isRtl = !isRtl;
 			caretIndex = if (!isRtl) __rtlOffset + caretIndex else caretIndex;
@@ -144,16 +259,9 @@ class FlxInputTextRTL extends FlxInputText
 
 
 				if (newText.length > 0 && (maxLength == 0 || (text.length + newText.length) < maxLength)) {
-					#if html5
-					if (isRtl) {
-						__rtlOffset++;
-						text = insertSubstring(text, newText, caretIndex - 1);
-					}
-					#else
 					caretIndex = if (isRtl) caretIndex-- else caretIndex = text.length;			
 					text = insertSubstring(text, newText, caretIndex);
 					caretIndex = if (!isRtl) caretIndex = text.length else caretIndex;	
-					#end
 	
 					text = text; // forces scroll update
 					onChange(FlxInputText.INPUT_ACTION);
@@ -161,126 +269,59 @@ class FlxInputTextRTL extends FlxInputText
 			}
 		}
 	}
+	#if js
+	var textInput:js.html.InputElement;
 
-	var charMap:Map<Int, String> = [
-		113 => "/",
-		119 => "'",
-		101 => "ק",
-		114 => "ר",
-		116 => "א",
-		121 => "ט",
-		117 => "ו",
-		105 => "ן",
-		111 => "ם",
-		112 => "פ",
-		97 => "ש",
-		115 => "ד",
-		100 => "ג",
-		103 => "ע",
-		104 => "י",
-		106 => "ח",
-		107 => "ל",
-		108 => "ך",
-		59 => "ף",
-		122 => "ז",
-		120 => "ס",
-		99 => "ב",
-		118 => "ה",
-		98 => "נ",
-		110 => "מ",
-		109 => "צ",
-		60 => '>',
-		62 => "<",
-		#if hl 46 #else 44 #end=> "ץ",
-		#if hl 44 #else 46 #end=> "ת",
-		102 => "כ",
-		32 => "",
-		47 => ".",
-		63 => "?",
-		39 => ",",
-		96 => ";",
-	];
+	function getInput()
+	{
+		textInput = cast js.Browser.document.createElement('input');
+		textInput.type = 'text';
+		textInput.style.position = 'absolute';
+		textInput.style.opacity = "0";
+		textInput.style.color = "transparent";
+		textInput.value = String.fromCharCode(127);
+		textInput.style.left = "0px";
+		textInput.style.top = "50%";
+		untyped (textInput.style).pointerEvents = 'none';
+		textInput.style.zIndex = "-10000000";
+		js.Browser.document.body.appendChild(textInput);
+		textInput.addEventListener('input', (e:js.html.InputEvent) -> {
+			var text = textInput.value;
+		}, true);
+	}
 
-	var englishMap:Map<Int, String> = [
-		113 => "q",
-		119 => "w",
-		101 => "e",
-		114 => "r",
-		116 => "t",
-		121 => "y",
-		117 => "u",
-		105 => "i",
-		111 => "o",
-		112 => "p",
-		97 => "a",
-		115 => "s",
-		100 => "d",
-		103 => "g",
-		104 => "h",
-		106 => "j",
-		107 => "k",
-		108 => "l",
-		59 => ";",
-		122 => "z",
-		120 => "x",
-		99 => "c",
-		118 => "v",
-		98 => "b",
-		110 => "n",
-		109 => "m",
-		60 => '>',
-		62 => "<",
-		44 => ",",
-		46 => ".",
-		102 => "f",
-		32 => "",
-		47 => "/",
-		63 => "?",
-		39 => "'",
-		96 => "`",
-	];
+	function updateFocus()
+	{
+		textInput.focus();
+		textInput.select();
+	}
+	#end
+
+	
 
 
 
 	function mapCharCode(charCode:Int):String
 	{
-		return if (isRtl) charMap[charCode] else englishMap[charCode];
+		return if (isRtl) FlxCharMaps.hebrewKeyMap[charCode] else FlxCharMaps.englishKeyMap[charCode];
 	}
+	
 	#if FLX_KEYBOARD
 	public override function update(elapsed:Float) {
 		super.update(elapsed);
+
 		if (hasFocus) {
 			if (FlxG.keys.justPressed.SPACE) {
 				pressSpace();
 			}
 		}
+
+		#if js
+		updateFocus();
+		#end
 	}
 	#end
-}
 
-private final class ExtraUtils extends FlxBasic {
-
-	var textRTL:FlxInputTextRTL;
-
-	public var backgroundVisible(default, set):Bool;
 	
-	function set_backgroundVisible(backgroundVisible:Bool):Bool {
-		textRTL.backgroundColor = FlxColor.TRANSPARENT;
-		return backgroundVisible;
-	}
-
-	public var borderVisible(default, set):Bool;
-
-	function set_borderVisible(borderVisible:Bool):Bool {
-		textRTL.fieldBorderColor = FlxColor.TRANSPARENT;
-		return borderVisible;
-	}
-
-	public function new(inputText:FlxInputTextRTL) {
-		super();
-		this.textRTL = inputText;
-		
-	}
-
 }
 #end
